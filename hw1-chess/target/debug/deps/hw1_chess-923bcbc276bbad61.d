0d962e6c04c66a3688dcf0deca0470d09c2dc90f
Tim White <tfy21twe@cs.umu.se>
2022-09-11 10:34:35 +0200

    Created an enum for pieces and a visual board representation in string format

---
+++ hw1-chess/target/debug/deps/hw1_chess-923bcbc276bbad61.d
@@ -0,0 +1,5 @@
+/home/tfy21twe/twhite-chess/hw1-chess/target/debug/deps/hw1_chess-923bcbc276bbad61: src/main.rs
+
+/home/tfy21twe/twhite-chess/hw1-chess/target/debug/deps/hw1_chess-923bcbc276bbad61.d: src/main.rs
+
+src/main.rs:
---
+++ hw1-chess/target/debug/hw1-chess.d
@@ -0,0 +1,1 @@
+/home/tfy21twe/twhite-chess/hw1-chess/target/debug/hw1-chess: /home/tfy21twe/twhite-chess/hw1-chess/src/main.rs
